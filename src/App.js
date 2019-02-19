a1885501f6ded6988b6f5ed4bb00e84d5ea5105e
cdesilva <chris.a.desilva@gmail.com>
2019-02-19 14:49:22 -0600

    Reset original scoreboard

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import GameBoard from './components/GameBoard'
-import Scoreboard2 from './components/Scoreboard2'
+import Scoreboard from './components/Scoreboard'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <GameBoard />
-        <Scoreboard2 />
+        <Scoreboard />
       </div>
     );
   }
