e4c67a4a82f7293d3c6f58e6d6d71ef1b9ee4399
cdesilva <chris.a.desilva@gmail.com>
2019-04-15 14:40:18 -0500

    Update GameBoard with new questions

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -11,35 +11,35 @@ class GameBoard extends Component {
             <div className="container pt-5">
             <h1 className="text-center mb-5">History Heroes</h1>
             <div className="row text-center">
-              <div className="col-sm border p-4">Progressive Leaders</div>
-              <div className="col-sm border p-4">Women's Rights</div>
-              <div className="col-sm border p-4">Worker's Rights</div>
-              <div className="col-sm border p-4">Political Reform</div>
-              <div className="col-sm border p-4">Amendments</div>
+              <div className="col-sm border p-4">G</div>
+              <div className="col-sm border p-4">R</div>
+              <div className="col-sm border p-4">E</div>
+              <div className="col-sm border p-4">A</div>
+              <div className="col-sm border p-4">T</div>
             </div>
             <div className="row text-center">
-              <div className="col-sm p-4"><BeginnerQuestions body={"Which president helped preserve more than 200 million acres of land?"} answer={"Theodore Roosevelt"} /></div>
-              <div className="col-sm p-4"><BeginnerQuestions body={"How did Alice Paul help improve life for women?"} answer={"Helped give women the right to vote"} /></div>
-              <div className="col-sm p-4"><BeginnerQuestions body={"What group of people was helped from the March of the Mill Children?"} answer={"Child workers"} /></div>
-              <div className="col-sm p-4"><BeginnerQuestions body={"In which state did Robert La Follette become governor to start his political reforms?"} answer={"Wisconsin"} /></div>
-              <div className="col-sm p-4"><BeginnerQuestions body={"The 18th Amendment was passed based on public support for prohibition. What became illegal after the amendment was passed?"} answer={"Alcohol"} /></div>
+              <div className="col-sm p-4"><BeginnerQuestions body={"What group of Americans gained the right to vote from the Nineteenth Amendment?"} answer={"Women"} /></div>
+              <div className="col-sm p-4"><BeginnerQuestions body={"What idea by Henry Ford made car manufacturing much more efficient?"} answer={"Assembly Line"} /></div>
+              <div className="col-sm p-4"><BeginnerQuestions body={"What neighborhood of New York City became a center of African American culture during the 1920s?"} answer={"Harlem"} /></div>
+              <div className="col-sm p-4"><BeginnerQuestions body={"What genre of music became extremely popular during the 1920s?"} answer={"Jazz"} /></div>
+              <div className="col-sm p-4"><BeginnerQuestions body={"The Prohibition movement led to the ban of what goods in the United States?"} answer={"Alcohol"} /></div>
             </div>
             <div className="row text-center">
-              <div className="col-sm p-4"><IntermediateQuestions body={"Which progressive leader helped start the NAACP?"} answer={"W.E.B. DuBois"} /></div>
-              <div className="col-sm p-4"><IntermediateQuestions body={"Who was the first woman elected to Congress?"} answer={"Jeannette Rankin"} /></div>
-              <div className="col-sm p-4"><IntermediateQuestions body={"In which state did the March of the Mill Children begin?"} answer={"Pennsylvania"} /></div>
-              <div className="col-sm p-4"><IntermediateQuestions body={"Give an example of a political position that could be subject to a recall."} answer={"State or local positions like mayor or governor"} /></div>
-              <div className="col-sm p-4"><IntermediateQuestions body={"Who could be directly elected after the 17th Amendment was passed?"} answer="Senators" /></div>
+              <div className="col-sm p-4"><IntermediateQuestions body={"What was the nickname given to the temporary towns built during the Great Depression?"} answer={"Hoovervilles"} /></div>
+              <div className="col-sm p-4"><IntermediateQuestions body={"What event triggered the start of the Great Depression?"} answer={"Stock market crash"} /></div>
+              <div className="col-sm p-4"><IntermediateQuestions body={"What group of people was hit hardest by the Dust Bowl"} answer={"Farmers"} /></div>
+              <div className="col-sm p-4"><IntermediateQuestions body={"Who was president at the start of the Great Depression?"} answer={"Herbert Hoover"} /></div>
+              <div className="col-sm p-4"><IntermediateQuestions body={"Which company became successful after starting a mail-order system for consumer goods?"} answer={"Sears"} /></div>
             </div>
             <div className="row text-center">
-              <div className="col-sm p-4"><AdvancedQuestions body={"In which national park did Theodore Roosevelt and John Muir have their famous camping trip?"} answer={"Yosemite"} /></div>
-              <div className="col-sm p-4"><AdvancedQuestions body={"Finish the quote from Alice Paul: '...to me there is nothing complicated about ordinary __________' "} answer={"equality"} /></div>
-              <div className="col-sm p-4"><AdvancedQuestions body={"In 1903, Oregon passed a law limiting the workday of women to how many hours?"} answer={"10"} /></div>
-              <div className="col-sm p-4"><AdvancedQuestions body={"What election system did Robert La Follette help create?"} answer={"Primary"} /></div>
-              <div className="col-sm p-4"><AdvancedQuestions body={"What was created under the 16th Amendment?"} answer={"Income tax"} /></div>
+              <div className="col-sm p-4"><AdvancedQuestions body={"What was the main goal of the Social Security Act?"} answer={"To help elderly people"} /></div>
+              <div className="col-sm p-4"><AdvancedQuestions body={"What event led to the Red Scare?"} answer={"Communist Revolution in Russia"} /></div>
+              <div className="col-sm p-4"><AdvancedQuestions body={"What was the name given to clubs that served alcohol illegally?"} answer={"Speakeasies"} /></div>
+              <div className="col-sm p-4"><AdvancedQuestions body={"The Immigration Act of 1924 banned people all immigration from where?"} answer={"Asia"} /></div>
+              <div className="col-sm p-4"><AdvancedQuestions body={"John Scopes was arrested for teaching what in school?"} answer={"Evolution"} /></div>
             </div>
             <div className="row text-center">
-                <div className="col-sm p-4"><FinalQuestion body={"What was the nickname given to the group of women that fought for the right to vote?"} answer={"Suffragettes"} /></div>
+                <div className="col-sm p-4"><FinalQuestion body={"What government agency was created to save failing banks and businesses?"} answer={"Reconstruction Finance Corporation (RFC)"} /></div>
             </div>
           </div>
         )
