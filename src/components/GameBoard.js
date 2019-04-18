f68d5e1a4e58894ad3a8f516b4a2b84d975c54db
cdesilva <chris.a.desilva@gmail.com>
2019-04-18 10:00:53 -0500

    Add form to input custom questions and answers

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,17 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Form } from 'react-bootstrap'
 import Questions from './Questions'
 import Categories from './Categories'
 
-const GameBoard = () => (
+const GameBoard = () => {
+
+  const [question, setQuestion] = useState('')
+  const [questionList, setQuestionList] = useState([])
+  const [answer, setAnswer] = useState([])
+
+  const onQuestionChange = e => {
+    e.preventDefault()
+    setQuestion(e.target.value)
+  }
+
+  const onAnswerChange = e => {
+    e.preventDefault()
+    setAnswer(e.target.value)
+  }
+
+  const handleAddQuestion = e => {
+    e.preventDefault()
+    setQuestionList([...questionList, {
+      body: question,
+      answer: answer,
+      variant: 'outline-primary',
+      level: 'Beginner'
+    }])
+    setQuestion('')
+    setAnswer('')
+  }
+
+         return (
             <div className="container pt-5">
               <h1 className="text-center mb-5">History Heroes</h1>
               <Categories />
+              <Form className="m-auto">
+                <Form.Group control="formBasicAddQuestion">
+                    <Form.Control value={question} onChange={onQuestionChange} type="text" placeholder="Enter new question" />
+                    <Form.Control value={answer} onChange={onAnswerChange} type="text" placeholder="Enter answer" />
+                </Form.Group>
+                <div className="row justify-content-center">
+                  <button onClick={handleAddQuestion} className="btn btn-outline-success mb-2">
+                  Add Question
+                  </button>
+                </div>
+              </Form>
               <div className="row text-center">
-                <div className="col-sm p-4"><Questions body={"What group of Americans gained the right to vote from the Nineteenth Amendment?"} answer={"Women"} variant={"outline-primary"} level={"Beginner"}/></div>
-                <div className="col-sm p-4"><Questions body={"What idea by Henry Ford made car manufacturing much more efficient?"} answer={"Assembly Line"} variant={"outline-primary"} level={"Beginner"}/></div>
-                <div className="col-sm p-4"><Questions body={"What neighborhood of New York City became a center of African American culture during the 1920s?"} answer={"Harlem"} variant={"outline-primary"} level={"Beginner"}/></div>
-                <div className="col-sm p-4"><Questions body={"What genre of music became extremely popular during the 1920s?"} answer={"Jazz"} variant={"outline-primary"} level={"Beginner"}/></div>
-                <div className="col-sm p-4"><Questions body={"The Prohibition movement led to the ban of what goods in the United States?"} answer={"Alcohol"} variant={"outline-primary"} level={"Beginner"}/></div>
+                {questionList.map( question => {
+                  return <div className="col-sm p-4"><Questions body={question.body} answer={question.answer} variant={question.variant} level={question.level}/></div>
+                })}
               </div>
               <div className="row text-center">
                 <div className="col-sm p-4"><Questions body={"What was the nickname given to the temporary towns built during the Great Depression?"} answer={"Hoovervilles"} variant={"outline-warning"} level={"Intermediate"} /></div>
@@ -31,7 +69,8 @@ const GameBoard = () => (
                   <div className="col-sm p-4"><Questions body={"What government agency was created by Herbert Hoover to save failing banks and businesses?"} answer={"Reconstruction Finance Corporation (RFC)"} variant={"danger"} level={"Final"} /></div>
               </div>
             </div>
-  )
+    )
+  }
 
 
 export default GameBoard
