6caf87f33e515bea6295194fab386aedab202b00
cdesilva <chris.a.desilva@gmail.com>
2019-04-26 11:48:50 -0500

    Add server variable

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+// Create a server, uses `handleRequest` which is function that takes
+// care of providing requested data
+const server = http.createServer(handleRequest);
+
+// Start the server
+server.listen(PORT, () => {
+  console.log('Server listening on: http://localhost:%s', PORT);
+});
+
 // create a GET route
 app.get('/express', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
